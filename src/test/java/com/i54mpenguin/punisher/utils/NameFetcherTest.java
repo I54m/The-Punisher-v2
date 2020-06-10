16d4ae0b6712c615726983e0a93b95513c2fa680
54mpenguin <54mpenguin@gmail.com>
2020-06-10 15:19:55 +1200

    Added another test and changed the #getName() test

--- src/test/java/com/i54mpenguin/punisher/utils/NameFetcherTest.java
+++ src/test/java/com/i54mpenguin/punisher/utils/NameFetcherTest.java
@@ -2,12 +2,17 @@ package com.i54mpenguin.punisher.utils;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NameFetcherTest {
 
     @Test
     void getName() {
+        assertEquals("54mpenguin", NameFetcher.getName("74f04a9bb7f9409da940b051f14dd3a5"));
+    }
+
+    @Test
+    void getStoredName() {
         NameFetcher.storeName("74f04a9bb7f9409da940b051f14dd3a5", "54mpenguin");
         assertEquals("54mpenguin", NameFetcher.getName("74f04a9bb7f9409da940b051f14dd3a5"));
     }
