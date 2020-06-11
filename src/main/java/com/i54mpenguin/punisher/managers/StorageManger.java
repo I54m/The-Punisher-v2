7149abd1670b86879c6386c1e88053038b9a0a72
54mpenguin <54mpenguin@gmail.com>
2020-06-11 17:54:29 +1200

    Added some of the required methods for a StorageManger

--- src/main/java/com/i54mpenguin/punisher/managers/StorageManger.java
+++ src/main/java/com/i54mpenguin/punisher/managers/StorageManger.java
@@ -1,4 +1,40 @@
 package com.i54mpenguin.punisher.managers;
 
+import com.i54mpenguin.punisher.objects.Punishment;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.UUID;
+
 public interface StorageManger {
+
+    void start() throws Exception;
+    void stop();
+
+    void setupStorage() throws Exception;
+    void startCaching();
+    void clearCache();
+    void resetCache();
+    void cache() throws Exception;
+    void dumpNew() throws Exception;
+    void updatePunishment(@NotNull Punishment punishment) throws Exception;
+
+    void createHistory(@NotNull UUID uuid) throws Exception;
+    void createStaffHistory(@NotNull UUID uuid) throws Exception;
+
+    void incrementHistory(@NotNull Punishment punishment) throws Exception;
+    void incrementStaffHistory(@NotNull Punishment punishment) throws Exception;
+
+    void loadUser(@NotNull UUID uuid) throws Exception;
+
+    int getOffences(@NotNull UUID targetUUID, @NotNull String reason) throws Exception;
+    TreeMap<Integer, Punishment> getHistory(UUID uuid) throws Exception;
+    TreeMap<Integer, Punishment> getStaffHistory(UUID uuid) throws Exception;
+    ArrayList<UUID> getAlts(UUID uuid) throws Exception;
+    TreeMap<Long, String> getIpHist(UUID uuid) throws Exception;
+
+    void updateAlts(UUID uuid, String ip) throws Exception;
+    void updateIpHist(UUID uuid, String ip) throws Exception;
+
 }
