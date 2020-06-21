e092b6209095b7cdf22645488780a2af6f6d270b
54mpenguin <54mpenguin@gmail.com>
2020-06-21 22:18:33 +1200

    Added support for dependent managers not having their super managers started

--- src/main/java/com/i54mpenguin/punisher/exceptions/ManagerNotStartedException.java
+++ src/main/java/com/i54mpenguin/punisher/exceptions/ManagerNotStartedException.java
@@ -2,7 +2,13 @@ package com.i54mpenguin.punisher.exceptions;
 
 public class ManagerNotStartedException extends Exception {
 
+    private Class<?> requiredManager = null;
     private final Class<?> manager;
+
+    public ManagerNotStartedException(Class<?> requiredManager, Class<?> manager) {
+        this.requiredManager = requiredManager;
+        this.manager = manager;
+    }
 
     public ManagerNotStartedException(Class<?> manager) {
         this.manager = manager;
@@ -10,7 +16,12 @@ public class ManagerNotStartedException extends Exception {
 
     @Override
     public String getMessage() {
-        return manager.getName() + " has not been started! It must be started using the provided start() method," +
+        if (requiredManager == null)
+            return manager.getName() + " has not been started! It must be started using the provided start() method," +
                 " this will initialize the manager correctly and ensure that only one instance of it is running at a time.";
+        else
+            return manager.getName() + " requires " + requiredManager.getName() + " to be started before it as " + manager.getName()
+                    + " depends on methods from " + requiredManager.getName() + " in order to be used correctly. " + requiredManager.getName()
+                    + " must be started using the provided start() method, this will initialize the manager correctly and ensure that only one instance of it is running at a time.";
     }
 }
