5f20a33837dbb81478b8d5495c68881d9f836509
54mpenguin <54mpenguin@gmail.com>
2020-06-21 22:13:55 +1200

    Recoded manager and implemented Manager interface

--- src/main/java/com/i54mpenguin/punisher/managers/ReputationManager.java
+++ src/main/java/com/i54mpenguin/punisher/managers/ReputationManager.java
@@ -1,86 +1,109 @@
 package com.i54mpenguin.punisher.managers;
 
-import com.i54mpenguin.punisher.PunisherPlugin;
+import com.i54mpenguin.punisher.exceptions.ManagerNotStartedException;
 import com.i54mpenguin.punisher.exceptions.PunishmentsDatabaseException;
-import com.i54mpenguin.punisher.handlers.ErrorHandler;
 import com.i54mpenguin.punisher.objects.Punishment;
+import com.i54mpenguin.punisher.utils.NameFetcher;
+import com.i54mpenguin.punisher.utils.UUIDFetcher;
+import lombok.Getter;
+import net.md_5.bungee.config.Configuration;
 
-import java.sql.SQLException;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
-public class ReputationManager {
-    // TODO: 20/06/2020 make a proper non static manager and possibly redo entire manager
-    private static final PunisherPlugin plugin = PunisherPlugin.getInstance();
-    private static final PunishmentManager punishMnger = PunishmentManager.getINSTANCE();
+public class ReputationManager implements Manager {
 
-    public static void minusRep(String targetname, String uuid, double amount) {
-//        if (!PunisherPlugin.reputationConfig.contains(uuid)) {
-//            PunisherPlugin.reputationConfig.set(uuid,  Double.valueOf(new DecimalFormat("##.##").format((5.0 - amount))));
-//        } else {
-//            double currentRep = PunisherPlugin.reputationConfig.getDouble(uuid);
-//            if ((currentRep - amount) > -10 && (currentRep - amount) < 10) {
-//                PunisherPlugin.reputationConfig.set(uuid,  Double.valueOf(new DecimalFormat("##.##").format((currentRep - amount))));
-//            } else if ((currentRep - amount) > 10) {
-//                PunisherPlugin.reputationConfig.set(uuid, 10.00);
-//            } else if ((currentRep - amount) <= -10) {
-//                PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format((currentRep - amount))));
-//                repBan(uuid, targetname);
-//            }
-//        }
-//        PunisherPlugin.saveRep();
+    @Getter
+    private static final ReputationManager INSTANCE = new ReputationManager();
+    private ReputationManager() {}
+
+    private static final PunishmentManager PUNISHMENT_MANAGER = PunishmentManager.getINSTANCE();
+    private static final PlayerDataManager PLAYER_DATA_MANAGER = PlayerDataManager.getINSTANCE();
+    private boolean locked = true;
+    private double banAt = -10.0;
+    private double startingRep = 5.0;
+
+    @Override
+    public void start() {
+        if (!locked) {
+            ERROR_HANDLER.log(new Exception("Repuation Manager Already started!"));
+            return;
+        }
+        if (PLAYER_DATA_MANAGER.isStarted()) {
+            banAt = PLUGIN.getConfig().getDouble("Reputation.Rep-Ban.Ban-At", -10);
+            startingRep = PLUGIN.getConfig().getDouble("Reputation.Starting-Rep", 5.0);
+            locked = false;
+        } else ERROR_HANDLER.log(new ManagerNotStartedException(PLAYER_DATA_MANAGER.getClass(), getClass()));
     }
 
-    public static void addRep(String targetname, String uuid, double amount) {
-//        if (!PunisherPlugin.reputationConfig.contains(uuid))
-//            PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format((5.0 + amount))));
-//        else {
-//            double currentRep = PunisherPlugin.reputationConfig.getDouble(uuid);
-//            if ((currentRep + amount) > -10 && (currentRep + amount) < 10)
-//                PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format((currentRep + amount))));
-//            else if ((currentRep + amount) > 10)
-//                PunisherPlugin.reputationConfig.set(uuid, 10.00);
-//            else if ((currentRep + amount) <= -10) {
-//                PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format((currentRep + amount))));
-//                repBan(uuid, targetname);
-//            }
-//        }
-//        PunisherPlugin.saveRep();
+    @Override
+    public boolean isStarted() {
+        return !locked;
     }
 
-    public static void setRep(String targetname, String uuid, double amount) {
-//        if (amount > 10)
-//            PunisherPlugin.reputationConfig.set(uuid, 10.00);
-//        else if (!(amount > -10)) {
-//            PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format(amount)));
-//            repBan(uuid, targetname);
-//        }else
-//            PunisherPlugin.reputationConfig.set(uuid, Double.valueOf(new DecimalFormat("##.##").format(amount)));
-//        PunisherPlugin.saveRep();
+    @Override
+    public void stop() {
+        if (locked) {
+            ERROR_HANDLER.log(new ManagerNotStartedException(this.getClass()));
+            return;
+        }
+        locked = true;
+    }
+
+    public void minusRep(UUID uuid, double amount) {
+        Configuration config = PLAYER_DATA_MANAGER.getPlayerData(uuid);
+        double currentRep = config.getDouble("Reputation", startingRep);
+        currentRep = currentRep - amount;
+        if (currentRep > 10.0)
+            currentRep = 10.0;
+        config.set("Reputation", currentRep);
+        PLAYER_DATA_MANAGER.savePlayerData(uuid);
+        if (PLUGIN.getConfig().getBoolean("Reputation.Rep-Ban.enabled") && currentRep <= banAt)
+            repBan(uuid);
+    }
+
+    public void addRep(UUID uuid, double amount) {
+        Configuration config = PLAYER_DATA_MANAGER.getPlayerData(uuid);
+        double currentRep = config.getDouble("Reputation", startingRep);
+        currentRep = currentRep + amount;
+        if (currentRep > 10.0)
+            currentRep = 10.0;
+        config.set("Reputation", currentRep);
+        PLAYER_DATA_MANAGER.savePlayerData(uuid);
+        if (PLUGIN.getConfig().getBoolean("Reputation.Rep-Ban.enabled") && currentRep <= banAt)
+            repBan(uuid);
+    }
+
+    public void setRep(UUID uuid, double amount) {
+        if (amount > 10.0)
+            amount = 10.0;
+        PLAYER_DATA_MANAGER.getPlayerData(uuid).set("Reputation", amount);
+        PLAYER_DATA_MANAGER.savePlayerData(uuid);
+        if (PLUGIN.getConfig().getBoolean("Reputation.Rep-Ban.enabled") && amount <= banAt)
+            repBan(uuid);
 
     }
 
-    public static String getRep(String uuid) {
-//        if (PunisherPlugin.reputationConfig.contains(uuid))
-//            return String.valueOf(PunisherPlugin.reputationConfig.getDouble(uuid));
-//        else return null;
-        return null;
+    public String getRep(UUID uuid) {
+        return String.valueOf(PLAYER_DATA_MANAGER.getPlayerData(uuid).getDouble("Reputation", startingRep));
     }
 
-    private static void repBan(String targetUUID, String targetName){
-        punishMnger.lock(targetUUID);
-        plugin.getProxy().getScheduler().schedule(plugin, () ->{
+    private void repBan(UUID targetUUID) {
+        PUNISHMENT_MANAGER.lock(targetUUID);
+        PLUGIN.getProxy().getScheduler().schedule(PLUGIN, () -> {
             try {
-                Punishment ban = new Punishment(Punishment.Type.BAN, Punishment.Reason.Custom, (long) 3.154e+12 + System.currentTimeMillis(), targetUUID, targetName, "CONSOLE", "Overly Toxic (Rep dropped below -10)");
-                punishMnger.issue(ban, null, false, true, false);
-            } catch (SQLException e) {
-                try {
-                    throw new PunishmentsDatabaseException("Issuing perm rep ban", targetName, ReputationManager.class.getName(), e);
-                } catch (PunishmentsDatabaseException pde) {
-                    ErrorHandler errorHandler = ErrorHandler.getINSTANCE();
-                    errorHandler.log(pde);
-                }
+                String targetName = NameFetcher.getName(targetUUID);
+                Punishment ban = new Punishment(Punishment.Type.BAN,
+                        "Rep-Ban",
+                        (long) 3.154e+12 + System.currentTimeMillis(),
+                        targetUUID,
+                        targetName,
+                        UUIDFetcher.getBLANK_UUID(),
+                        "Overly Toxic (Rep dropped below " + banAt + ")");
+                PUNISHMENT_MANAGER.issue(ban, null, false, true, false);
+            } catch (Exception e) {
+                ERROR_HANDLER.log(new PunishmentsDatabaseException("Issuing rep ban", NameFetcher.getName(targetUUID), ReputationManager.class.getName(), e));
             }
         }, 2, TimeUnit.SECONDS);
     }
-
 }
