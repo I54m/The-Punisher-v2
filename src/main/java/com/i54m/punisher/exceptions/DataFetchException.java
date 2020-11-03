475dc2bbb79ebef402fea90a1a1a4672441f589d
I54m <35064677+I54m@users.noreply.github.com>
2020-11-03 16:07:32 +1300

    Revised constructor, old one to be removed in next version

--- src/main/java/com/i54m/punisher/exceptions/DataFetchException.java
+++ src/main/java/com/i54m/punisher/exceptions/DataFetchException.java
@@ -1,17 +1,30 @@
 package com.i54m.punisher.exceptions;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 public class DataFetchException extends Exception {
 
     private final String reason, user, requestedInfo, causingClass;
     private final Throwable cause;
 
-    public DataFetchException(String reason, String user, String requestedInfo, String causingClass, Throwable cause){
+    @Deprecated
+    public DataFetchException(String reason, String user, String requestedInfo, String causingClass, Throwable cause){// TODO: 3/11/2020 shuffle args to make more sense
         super(cause);
         this.reason = reason;
         this.user = user;
         this.requestedInfo = requestedInfo;
         this.causingClass = causingClass;
         this.cause = cause;
+    }
+
+    public DataFetchException(@NotNull String causingClass, @NotNull String requestedInfo, @NotNull String user, @Nullable Throwable cause, @NotNull String reason){
+        super(cause);
+        this.causingClass = causingClass;
+        this.requestedInfo = requestedInfo;
+        this.user = user;
+        this.cause = cause;
+        this.reason = reason;
     }
 
     @Override
