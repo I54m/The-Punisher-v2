8da0c7273268ce5b6f5994568533eb10cf23aabc
I54m <35064677+I54m@users.noreply.github.com>
2020-11-09 16:53:11 +1300

    Updated todo

--- src/main/java/com/i54m/punisher/managers/storage/StorageManager.java
+++ src/main/java/com/i54m/punisher/managers/storage/StorageManager.java
@@ -17,7 +17,7 @@ public interface StorageManager extends Manager {// TODO: 9/11/2020 Added Javado
 
     TreeMap<Integer, Punishment> PUNISHMENT_CACHE = new TreeMap<>();
     Map<UUID, ActivePunishments> ACTIVE_PUNISHMENT_CACHE = new HashMap<>();
-    Map<String, Integer> PUNISHMENT_REASONS = new HashMap<>();// TODO: 9/11/2020 urmmm i forgot how I was gonna set this up... ik String = reason though but wtf did the int represent... expiration or offence number Maybe?
+    Map<String, Integer> PUNISHMENT_REASONS = new HashMap<>();// TODO: 9/11/2020 string = reason, int = max number of offences, all defined in punishments.yml
 
     default TreeMap<Integer, Punishment> getPunishmentCache() {
         return PUNISHMENT_CACHE;
