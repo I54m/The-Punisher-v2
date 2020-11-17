9069adb039f729673552f91638006f7d04621ac2
I54m <35064677+I54m@users.noreply.github.com>
2020-11-17 19:02:30 +1300

    Added support for different storageManagers

--- src/main/java/com/i54m/punisher/exceptions/PunishmentsStorageException.java
+++ src/main/java/com/i54m/punisher/exceptions/PunishmentsStorageException.java
@@ -1,12 +1,15 @@
 package com.i54m.punisher.exceptions;
 
-import java.sql.SQLException;
+import com.i54m.punisher.PunisherPlugin;
+import com.i54m.punisher.managers.storage.StorageManager;
+import com.i54m.punisher.managers.storage.StorageType;
 
-public class PunishmentsStorageException extends SQLException { // TODO: 5/08/2020 redo this error to support the different storage managers
+public class PunishmentsStorageException extends Exception {
     private final String reason, user, causingClass;
     private String command;
     private String[] commandline;
     private final Throwable cause;
+    private final StorageManager storageManager;
 
     public PunishmentsStorageException(String reason, String user, String causingClass, Throwable cause) {
         super(cause);
@@ -14,6 +17,7 @@ public class PunishmentsStorageException extends SQLException { // TODO: 5/08/20
         this.user = user;
         this.causingClass = causingClass;
         this.cause = cause;
+        this.storageManager = PunisherPlugin.getInstance().getStorageManager();
     }
 
     public PunishmentsStorageException(String reason, String user, String causingClass, Throwable cause, String command, String[] commandline) {
@@ -24,6 +28,7 @@ public class PunishmentsStorageException extends SQLException { // TODO: 5/08/20
         this.cause = cause;
         this.command = command;
         this.commandline = commandline;
+        this.storageManager = PunisherPlugin.getInstance().getStorageManager();
     }
 
     @Override
@@ -34,10 +39,10 @@ public class PunishmentsStorageException extends SQLException { // TODO: 5/08/20
                 for (String args : commandline) {
                     sb.append(args).append(" ");
                 }
-                return causingClass + ".class has failed multiple times to access the mysql database on user " + user + ", this was caused by " + cause
+                return causingClass + ".class has failed multiple times to access the " + StorageType.getStorageTypeFromManager(storageManager).name() + " database on user " + user + ", this was caused by " + cause
                         + ". Reason for database access request: " + reason + ". Command executed: " + command + " " + sb.toString() + ".";
             }
-            return causingClass + ".class has failed multiple times to access the mysql database on user " + user + ", this was caused by " + cause
+            return causingClass + ".class has failed multiple times to access the " + StorageType.getStorageTypeFromManager(storageManager).name() + " database on user " + user + ", this was caused by " + cause
                     + ". Reason for database access request: " + reason + ".";
         } else {
             if (command != null && commandline.length > 0) {
@@ -45,10 +50,10 @@ public class PunishmentsStorageException extends SQLException { // TODO: 5/08/20
                 for (String args : commandline) {
                     sb.append(args).append(" ");
                 }
-                return causingClass + ".class has failed to access the mysql database, this was caused by " + cause
+                return causingClass + ".class has failed to access the " + StorageType.getStorageTypeFromManager(storageManager).name() + " database, this was caused by " + cause
                         + ". Reason for database access request: " + reason + ". Command executed: " + command + " " + sb.toString() + ".";
             }
-            return causingClass + ".class has failed to access the mysql database, this was caused by " + cause
+            return causingClass + ".class has failed to access the " + StorageType.getStorageTypeFromManager(storageManager).name() + " database, this was caused by " + cause
                     + ", Cause message: " + cause.getMessage() + ". Reason for database access request: " + reason + ".";
         }
     }
