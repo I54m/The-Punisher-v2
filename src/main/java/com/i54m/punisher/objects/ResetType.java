134c7eb737756516ec59d207d5c5289ea195f9d9
I54m <54mpenguin@gmail.com>
2021-01-17 19:11:29 +1300

    Added ResetType to help identify the type of reset to process

---
+++ src/main/java/com/i54m/punisher/objects/ResetType.java
@@ -0,0 +1,5 @@
+package com.i54m.punisher.objects;
+
+public enum ResetType {// TODO: 8/01/2021 expand on this
+    Bans, Mutes, Kicks, Warns, Punishments, IpHist, Alts, PlayerData, Reputation, Everything, History
+}
